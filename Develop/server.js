2c012105197855a772f87c0eece01144122cdebe
Dixon <zdixon91.zd@gmail.com>
2022-08-20 01:03:26 -0500

    added html route to server

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const apiRoutes = require ('./routes/apiRoutes');
+const htmlRoutes = require ('./routes/htmlRoutes')
 const PORT = process.env.PORT || 3001;
 
 const app = express();
