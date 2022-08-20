d151a59aec98e43f0e72359213092e15d66f2028
Dixon <zdixon91.zd@gmail.com>
2022-08-20 00:33:34 -0500

    server set up

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const apiRoutes = require ('./routes/apiRoutes');
-const
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use('/api', apiRoutes);
+
+app.listen(PORT, () => {
+    console.log('Sever now running on port ${PORT}!');
+});
